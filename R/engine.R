ac22b75348b23b3855378bfbfd68d1099308ecb4
Kane Cullimore <kanecullimore@yahoo.com>
2020-06-29 16:01:29 -0700

    Added MacOS support

--- R/engine.R
+++ R/engine.R
@@ -41,13 +41,14 @@ dockerRunCmd <- function(dir, image_tag, headless, port) {
         ## Container name
         name <- "rselenium-container-mac"
         ## Get IP info
+        HOME <- system("echo $HOME", intern=TRUE)
         IP <- system("ipconfig getifaddr en0", intern=TRUE)
         ## Link docker to host display
         ## NOTE: Xquartz X11 Preferences/Security set "Allow connections from network clients"
         display_setup <- paste(paste0("--env DISPLAY=", IP, ":0"),
                                "--env XAUTHORITY=/.Xauthority",
                                "--volume /tmp/.X11-unix:/tmp/.X11-unix",
-                               "--volume ~./Xauthority:/.Xauthority")
+                               paste0("--volume ", HOME, "/.Xauthority:/.Xauthority"))
     } else {
         stop("Only Linux OS & MacOS is supported by RSeleniumEngine at this time.")
     }
